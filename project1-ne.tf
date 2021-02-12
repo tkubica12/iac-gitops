cb1184334a2d74b6220f4f07160ada9b5c72e4b8
Tomas Kubica <tkubica12@gmail.com>
2021-02-12 16:08:29 +0100

    fix module naming

--- project1-ne.tf
+++ project1-ne.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "project1" {
+resource "azurerm_resource_group" "project1-ne" {
   name     = "project1-ne-rg"
   location = "westeurope"
 }
 
-module "kubernetes" {
+module "kubernetes-project1-ne" {
   source = "./modules/kubernetes"
 
-  resource_group  = azurerm_resource_group.project1.name
-  location        = azurerm_resource_group.project1.location
+  resource_group  = azurerm_resource_group.project1-ne.name
+  location        = azurerm_resource_group.project1-ne.location
   name            = "project1-aks-ne"
   subnet_id       = azurerm_subnet.ne-project1.id
   admin_group_id  = "2f003f7d-d039-4f87-8575-c2d45d091c2c"
--- project1-we.tf
+++ project1-we.tf
@@ -1,13 +1,13 @@
-resource "azurerm_resource_group" "project1" {
+resource "azurerm_resource_group" "project1-we" {
   name     = "project1-we-rg"
   location = "westeurope"
 }
 
-module "kubernetes" {
+module "kubernetes-project1-we" {
   source = "./modules/kubernetes"
 
-  resource_group  = azurerm_resource_group.project1.name
-  location        = azurerm_resource_group.project1.location
+  resource_group  = azurerm_resource_group.project1-we.name
+  location        = azurerm_resource_group.project1-we.location
   name            = "project1-aks-we"
   subnet_id       = azurerm_subnet.we-project1.id
   admin_group_id  = "2f003f7d-d039-4f87-8575-c2d45d091c2c"
