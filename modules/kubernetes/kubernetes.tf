6c4d31109f295a9a8ff126d10c4a3ff8fc7120f9
Tomas Kubica <tkubica12@gmail.com>
2021-02-12 14:01:38 +0100

    add gitops secret for app part

--- modules/kubernetes/kubernetes.tf
+++ modules/kubernetes/kubernetes.tf
@@ -125,6 +125,21 @@ resource "kubernetes_secret" "common" {
   }
 }
 
+resource "kubernetes_secret" "app" {
+  depends_on = [kubectl_manifest.apply, azurerm_kubernetes_cluster.demo]
+
+  metadata {
+    name      = data.flux_sync.app.name
+    namespace = data.flux_sync.app.namespace
+  }
+
+  data = {
+    identity       = base64decode(var.GIT_PRIVATE_KEY)
+    "identity.pub" = base64decode(var.GIT_PUBLIC_KEY)
+    known_hosts    = "github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=="
+  }
+}
+
 # Split multi-doc YAML with
 # https://registry.terraform.io/providers/gavinbunney/kubectl/latest
 data "kubectl_file_documents" "apply" {
