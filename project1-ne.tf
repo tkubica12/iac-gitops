1d7b0c13431e704b0781f0ebe8aa9cc69e58c426
Tomas Kubica <tkubica12@gmail.com>
2021-02-12 15:19:12 +0100

    add ne cluster and public ip dns

---
+++ project1-ne.tf
@@ -0,0 +1,16 @@
+resource "azurerm_resource_group" "project1" {
+  name     = "project1-rg"
+  location = "westeurope"
+}
+
+module "kubernetes" {
+  source = "./modules/kubernetes"
+
+  resource_group  = azurerm_resource_group.project1.name
+  location        = azurerm_resource_group.project1.location
+  name            = "project1-aks-ne"
+  subnet_id       = azurerm_subnet.ne-project1.id
+  admin_group_id  = "2f003f7d-d039-4f87-8575-c2d45d091c2c"
+  GIT_PRIVATE_KEY = var.GIT_PRIVATE_KEY
+  GIT_PUBLIC_KEY  = var.GIT_PUBLIC_KEY
+}
